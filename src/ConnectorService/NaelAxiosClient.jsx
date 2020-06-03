137811989598c94ed886cf125fb7aa736babaa8a
Nathanael <nael.nathanael71@gmail.com>
2020-06-03 11:19:46 +0800

    rework backend connector

--- src/ConnectorService/NaelAxiosClient.jsx
+++ src/ConnectorService/NaelAxiosClient.jsx
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: "http://localhost:8080"
-    // baseURL: "http://api.nathanael.my.id"
+    baseURL: "http://api.nathanael.my.id"
 })
 
 export default instance;
